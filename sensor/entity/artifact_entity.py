fc8bf98da159fa77f0dd13964769b9742ca95670
Gopal060 <gopalrkate@gmail.com>
2024-09-03 15:40:29 +0530

    Updated upto Model Training

---
+++ sensor/entity/artifact_entity.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+
+"""  @dataclass is used to store data. """
+
+# This DataIngestionArtifact stores the file paths of the training and test datasets.
+@dataclass
+class DataIngestionArtifact:
+    trained_file_path: str
+    test_file_path: str
+
+
+# This DataValidationArtifact stores the file paths, validation status and drift report of the training and test datasets.
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
+
+# This DataTransformationArtifact stores the file paths of the training and test datasets in the form of numpy array and Transformed object which contains all steps in transformation.
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+
+# This artifact encapsulates the performance metrics of the model, specifically for classification tasks.
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score: float
+    precision_score: float
+    recall_score: float
+
+
+# This artifact contains the path to the trained model file and the associated training and testing metrics.
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
+    train_metric_artifact: ClassificationMetricArtifact
+    test_metric_artifact: ClassificationMetricArtifact
